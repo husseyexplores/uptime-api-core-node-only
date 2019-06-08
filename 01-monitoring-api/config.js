3864bc84d599c379ef2f8785015f47797aa1c69f
husseyexplores <husseyexplores@gmail.com>
2019-06-08 15:33:12 +0500

    Add hashSalt

--- 01-monitoring-api/config.js
+++ 01-monitoring-api/config.js
@@ -5,6 +5,7 @@ environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
   envName: 'staging',
+  hashSalt: 'thisIsAsuperSecretHashingSalt',
 }
 
 //  Production env
@@ -12,6 +13,7 @@ environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
   envName: 'production',
+  hashSalt: 'thisIsAsuperSecretHashingSalt',
 }
 
 const currentEnv = typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLowerCase() : ''
